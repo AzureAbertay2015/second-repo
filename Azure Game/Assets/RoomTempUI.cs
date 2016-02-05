b5ccc022c836c010ab1f41769a7fab140db884e2
adamwright95 <adam.wright.1@hotmail.co.uk>
2016-02-05 16:09:29 +0000

    Added room temperature to ui

---
+++ Azure Game/Assets/RoomTempUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RoomTempUI : MonoBehaviour {
+
+    private Text m_Roomtemptext;
+    public GameManager m_Gamemanager;
+    int m_Roomtemp;
+
+	// Use this for initialization
+	void Start () {
+
+        m_Roomtemptext = GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_Roomtemp = (int)m_Gamemanager.m_Roomtemp;
+        m_Roomtemptext.text = m_Roomtemp.ToString() + "C";
+	
+	}
+}
