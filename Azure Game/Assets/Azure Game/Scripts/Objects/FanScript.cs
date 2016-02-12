8d901bf63a24babb11c336aa77adfce38cb166c8
Peter Terrell <terrelllo@outlook.com>
2016-02-12 19:58:01 +0000

    'Merge' fixed gameobject code from pt2 branch

--- Azure Game/Assets/Azure Game/Scripts/Objects/FanScript.cs
+++ Azure Game/Assets/Azure Game/Scripts/Objects/FanScript.cs
@@ -5,12 +5,12 @@ public class FanScript : MonoBehaviour {
 
 	//public Vector3 m_FanForce;
 	public float m_FanForce;
-	public Player m_Player;
-	public GameManager m_GameManager;
+	private Player m_Player;
+	private GameRules m_GameRules;
 
 	// Use this for initialization
 	void Start () {
-		
+		m_Player = GameManager.GetPlayer();
 	}
 	
 	// Update is called once per frame
@@ -22,7 +22,7 @@ public class FanScript : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "Player")
 		{
-			if (m_GameManager.m_State == GameManager.PlayerState.Gas)
+			if (m_Player.GetState() == Player.State.Gas)
 			{
 				other.gameObject.GetComponent<Rigidbody>().AddForce(GetComponentInParent<Transform>().up * m_FanForce);
 			}			
