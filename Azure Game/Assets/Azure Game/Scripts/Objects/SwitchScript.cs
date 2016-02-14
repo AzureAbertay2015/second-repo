78613f34d34a5cf095e821a29fac0bf5bf96000c
Peter Terrell <terrelllo@outlook.com>
2016-02-14 15:08:37 +0000

    Improved switch script
    Switch rotation now done around local axis, letting the object be
    rotated without breaking the switch animation

--- Azure Game/Assets/Azure Game/Scripts/Objects/SwitchScript.cs
+++ Azure Game/Assets/Azure Game/Scripts/Objects/SwitchScript.cs
@@ -2,10 +2,6 @@
 using System.Collections;
 
 public class SwitchScript : MonoBehaviour {
-
-	public Quaternion m_OpenRotation;
-	public Quaternion m_ClosedRotation;
-
 	public bool m_SwitchState;
 
 	public enum SwitchType { AirConditioner, Heater };
@@ -13,8 +9,7 @@ public class SwitchScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		m_OpenRotation = Quaternion.Euler(6, 0, 0);
-		m_ClosedRotation = Quaternion.Euler(354, 0, 0);
+		this.transform.GetChild(0).gameObject.transform.Rotate(-6, 0, 0, Space.Self);
 		m_SwitchState = false;	
 	}
 	
@@ -28,7 +23,7 @@ public class SwitchScript : MonoBehaviour {
 	{
 		m_SwitchState = true;
 		GameObject o = this.transform.GetChild(0).gameObject;
-		o.transform.rotation = m_OpenRotation;
+		o.transform.Rotate(12, 0, 0, Space.Self);
 		if (m_SwitchType == SwitchType.AirConditioner) {
 			GameManager.GetGameRules().CoolDownRoom();
 		}
@@ -41,7 +36,7 @@ public class SwitchScript : MonoBehaviour {
 	{
 		m_SwitchState = false;
 		GameObject o = this.transform.GetChild(0).gameObject;
-		o.transform.rotation = m_ClosedRotation;
+		o.transform.Rotate(348, 0, 0, Space.Self);
 		if (m_SwitchType == SwitchType.AirConditioner)
 		{
 			GameManager.GetGameRules().HeatUpRoom();
