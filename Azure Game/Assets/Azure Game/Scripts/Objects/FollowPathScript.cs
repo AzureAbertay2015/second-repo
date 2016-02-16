b37f8b37750fa89587eca2096b9bdf06bdd5b963
Peter Terrell <terrelllo@outlook.com>
2016-02-16 14:22:38 +0000

    Moving traps with iTween and iTweenPath
    - iTween and iTweenPath are free and under BSD license
    - Visual path edit and one function call to follow
    - Added a piston trap and prefab
    - Added follow path/piston path follow scripts
    - Created a scene to build and test objects, as Game Scene was getting
    too busy

---
+++ Azure Game/Assets/Azure Game/Scripts/Objects/FollowPathScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPathScript : MonoBehaviour {
+
+	public float m_Speed;
+	//bool atpoint1, atpoint2;
+
+	// Use this for initialization
+	void Start()
+	{
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("trap_path"), "looptype", iTween.LoopType.loop, "time", m_Speed, "easetype", iTween.EaseType.linear, "movetopath", false, "delay", 0));
+	}
+}
---
+++ Azure Game/Assets/Azure Game/Scripts/Objects/PistonPathScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistonPathScript : MonoBehaviour {
+
+	public float m_Speed;
+	//bool atpoint1, atpoint2;
+
+	// Use this for initialization
+	void Start()
+	{
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("path"), "looptype", iTween.LoopType.none, "time", m_Speed, "easetype", iTween.EaseType.easeOutBounce, "movetopath", false, "delay", 0));
+	}
+}
