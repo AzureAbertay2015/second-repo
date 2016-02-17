0d67be0bd73e1c83b81d56525c8b2adefe0980d5
Finlay <thumpster72@gmail.com>
2016-02-17 18:51:40 +0000

    Tutorial Text Appears and disappears

--- Azure Game/Assets/Azure Game/Scripts/Game/TutorialScript.cs
+++ Azure Game/Assets/Azure Game/Scripts/Game/TutorialScript.cs
@@ -6,18 +6,29 @@ using System;
 public class TutorialScript : MonoBehaviour {
     
     public String m_tutorialDialog;
+    private bool m_triggeredDialog;
     
     void Start()
     {
-
+        m_triggeredDialog = false;
     }
 
     //called when an object collides with this
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && !m_triggeredDialog)
         {
-            GameManager.GetGameRules().ToggleTutorial(m_tutorialDialog);      
+            GameManager.GetGameRules().ToggleTutorial(m_tutorialDialog);
+            m_triggeredDialog = true;   
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && m_triggeredDialog == true)
+        {
+            m_tutorialDialog = " ";
+            GameManager.GetGameRules().ToggleTutorial(m_tutorialDialog);
         }
     }
 }
