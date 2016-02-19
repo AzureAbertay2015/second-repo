86a76bf188808a1906c284b7a0db5d47d1fef8af
adamwright95 <adam.wright.1@hotmail.co.uk>
2016-02-19 14:26:48 +0000

    Fixed issues after merging with master branch

--- Azure Game/Assets/Azure Game/Scripts/Objects/SwitchScript.cs
+++ Azure Game/Assets/Azure Game/Scripts/Objects/SwitchScript.cs
@@ -11,6 +11,9 @@ public class SwitchScript : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+
+        SwitchOff();
+
 		transform.GetChild(0).gameObject.transform.Rotate(6, 0, 0, Space.Self);
 		m_SwitchState = true;
 		if (m_SwitchType == SwitchType.Heater)
