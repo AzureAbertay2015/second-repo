4166fdf13709919ea9cb53175b2943b953472354
Finlay <thumpster72@gmail.com>
2016-02-24 18:33:05 +0000

    Setup options menu
    Populating dropdown list with available resolutions.

---
+++ Azure Game/Assets/Azure Game/Scripts/Menu/PopulateResolutionsScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PopulateResolutionsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Dropdown dropdown = GetComponent<Dropdown>();
+        Resolution[] resolutions = Screen.resolutions;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            dropdown.options[i].text = resolutions[i].width + " x " + resolutions[i].height;// ResToString(resolutions[i]);
+            dropdown.value = i;
+
+            dropdown.onValueChanged.AddListener(delegate { Screen.SetResolution(resolutions[dropdown.value].width, resolutions[dropdown.value].height, true); });
+        }
+    }
+    string ResToString(Resolution res)
+    {
+        return res.width + " x " + res.height;
+    }
+}
---
+++ Azure Game/Assets/Azure Game/Scripts/Menu/SelectResolutionScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectResolutionScript : MonoBehaviour {
+
+	public void SelectResolution(int option)
+    {
+
+    }
+}
