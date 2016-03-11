d60559b2b6d3718882e176982dc7d315c35d2a7b
adamwright95 <adam.wright.1@hotmail.co.uk>
2016-03-11 11:55:56 +0000

    added checkpoints

---
+++ Azure Game/Assets/Azure Game/Scripts/Game/Checkpoint.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    public bool activated = false;
+    public static GameObject[] m_Checkpoints;
+
+
+	// Use this for initialization
+	void Start () {
+
+        m_Checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+	
+	}
+
+    void Awake()
+    {
+      DontDestroyOnLoad(this);    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void ActivateCheckpoint()
+    {
+
+        foreach(GameObject checkpt in m_Checkpoints)
+        {
+            checkpt.GetComponent<Checkpoint>().activated = false;
+        }
+
+        activated = true;
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        {
+            ActivateCheckpoint();
+        }
+    }
+
+    public Vector3 GetActiveCheckPoints()
+    {
+        Vector3 result = new Vector3(-3.5f, 26.29f, -1.11f);
+
+        if(m_Checkpoints != null)
+        {
+            foreach(GameObject checkpt in m_Checkpoints)
+            {
+                if(checkpt.GetComponent<Checkpoint>().activated)
+                {
+                    result = checkpt.transform.position;
+                    break;
+                }
+            }
+        }
+  
+        return result;
+    }
+
+    public GameObject[] GetCheckpoints()
+    {
+        return m_Checkpoints;
+    }
+
+    public void SetCheckpoints(GameObject[] m_Checkpointlist)
+    {
+        m_Checkpoints = m_Checkpointlist;
+    }
+}
