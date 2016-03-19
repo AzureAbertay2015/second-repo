967b976c1d902c682029f993cc0f65f957396089
Peter Terrell <terrelllo@outlook.com>
2016-03-19 22:07:13 +0000

    implemented basic pick up with test GUI

---
+++ Azure Game/Assets/Azure Game/Scripts/Objects/Collectible.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectible : MonoBehaviour {
+
+    public enum CollectibleType {Null, SpeedUp, SpeedDown, Time, PointsUp, PointsDown, Charge, Puzzle};
+    public CollectibleType m_Collectible;
+    private PlayerState m_PlayerState;
+    
+	// Use this for initialization
+	void Start () {
+        if (m_Collectible == CollectibleType.Null)
+        {
+            Debug.LogError(name + " is " + m_Collectible);
+        }
+        m_PlayerState = GameManager.GetPlayer().GetComponent<PlayerState>();
+        
+	}
+	
+    public void Consume()
+    {
+        Debug.Log("Collectible consumed");
+        switch (m_Collectible)
+        {
+            case CollectibleType.SpeedUp:
+                m_PlayerState.SpeedUp();
+                break;
+
+            case CollectibleType.SpeedDown:
+                m_PlayerState.SpeedDown();
+                break;
+
+            case CollectibleType.Time:
+                m_PlayerState.Time();
+                break;
+
+            case CollectibleType.PointsUp:
+                m_PlayerState.PointsUp();
+                break;
+
+            case CollectibleType.PointsDown:
+                m_PlayerState.PointsDown();
+                break;
+
+            case CollectibleType.Charge:
+                m_PlayerState.Charge();
+                break;
+
+            case CollectibleType.Puzzle:
+                m_PlayerState.Puzzle();
+                break;
+
+            default:
+                Debug.Log(m_Collectible + "has gone awry");
+                break;
+        }
+
+    }
+
+
+   public CollectibleType GetCollectibleType()
+    {
+        return m_Collectible;
+    }
+
+    private void SetCanvasValues()
+    {
+    }
+}
---
+++ Azure Game/Assets/Azure Game/Scripts/Player/PlayerState.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerState : MonoBehaviour {
+
+    public Text m_SpeedText;
+    public Text m_TimeText;
+    public Text m_PointsText;
+    public Text m_ChargeText;
+    public Text m_PuzzleText;
+
+    private int m_Speed;
+    private int m_Time;
+    private int m_Points;
+    private int m_Charge;
+    private int m_PuzzlePieces;
+
+
+    void Start()
+    {
+        m_Speed = 50;
+        m_Time = 50;
+        m_Points = 0;
+        m_Charge = 0;
+        m_PuzzlePieces = 0;
+
+        SetUIStrings();
+    }
+
+    void Update()
+    {
+        SetUIStrings();
+    }
+
+    private void SetUIStrings()
+    {
+        m_SpeedText.text = "Speed: " + m_Speed;
+        m_TimeText.text = "Time: " + m_Time;
+        m_PointsText.text = "Points: " + m_Points;
+        m_ChargeText.text = "Charge: " + m_Charge;
+        m_PuzzleText.text = "Puzzle: " + m_PuzzlePieces;
+    }
+
+    public void SpeedUp()
+    {
+        // increase player speed
+        m_Speed += 5;
+    }
+    
+    public void SpeedDown()
+    {
+        // decrease player speed
+        m_Speed -= 5;
+    }
+
+    public void Time()
+    {
+        // Add time to the clock
+        m_Time += 10;
+    }
+
+    public void PointsUp()
+    {
+        // add points to the score
+        m_Points += 1;
+    }
+
+    public void PointsDown()
+    {
+        // add points to the score
+        m_Points -= 1;
+    }
+
+    public void Charge()
+    {
+        // Add charge to the bar
+        m_Charge += 10;
+    }
+
+    public void Puzzle()
+    {
+        // collect a puzzle piece
+        m_PuzzlePieces += 1;
+    }
+
+}
